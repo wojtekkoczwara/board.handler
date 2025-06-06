afa229a50ce28111ed53916b2101418a05f295d8
Wojtek <wojtekkoczwara@gmail.com>
2025-06-06 12:57:01 +0200

    email validation

--- src/main/java/com/hublocal/board/handler/repository/UserRepository.java
+++ src/main/java/com/hublocal/board/handler/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.hublocal.board.handler.repository;
 
 import com.hublocal.board.handler.entities.Users;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,8 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByUserName(String name);
+    Optional<Users> findByEmail(String email);
+
+
 
 }
